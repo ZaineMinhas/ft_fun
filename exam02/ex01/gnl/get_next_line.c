43f23534a5b6cb50c882e33c301fe41c6e52d399
ZaineMinhas <zaineminhas5@gmail.com>
2021-11-10 17:34:52 +0100

    GROS COMMIT POUR GROS PUSH

--- exam02/ex01/gnl/get_next_line.c
+++ exam02/ex01/gnl/get_next_line.c
@@ -4,7 +4,7 @@ char	*ft_check(int fd)
 {
 	char	*buff;
 
-	if (fd < 0 || BUFFER_SIZE < 1)
+	if (fd < 0 || fd > 10240 || BUFFER_SIZE < 1)
 		return (NULL);
 	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!buff)
@@ -12,19 +12,7 @@ char	*ft_check(int fd)
 	return (buff);
 }
 
-int	ft_strlen(char *str)
-{
-	int	i;
-	
-	if (!str)
-		return (0);
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-int	ft_index(char *str, char c)
+int		ft_index(char *str, char c)
 {
 	int	i;
 
@@ -34,65 +22,103 @@ int	ft_index(char *str, char c)
 	while (str[++i])
 		if (str[i] == c)
 			return (i);
-	if (str[i] == c)
-		return (i);
 	return (-1);
 }
 
-char	*ft_gnljoin(char *str1, char *str2)
+int		ft_strlen(char *str)
+{
+	int	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*gnljoin(char *str1, char *str2)
 {
 	char	*dest;
 	int		size;
 	int		i;
 
-	size = ft_strlen(str1) + ft_strlen(str2);
-	dest = malloc(sizeof(char) * size + 1);
-	if (!dest)
-		return (NULL);
+	 size = ft_strlen(str1) + ft_strlen(str2) + 1;
+	 dest = malloc(sizeof(char) * size);
+	 if (!dest)
+	 	return (NULL);
 	i = -1;
 	if (str1)
 		while (str1[++i])
 			dest[i] = str1[i];
 	size = -1;
-	if (str2)
-	{
-		if (i == -1)
-			i = 0;
-		while (str2[++size])
-			dest[i + size] = str2[size];
-	}
+	if (i == -1)
+		i = 0;
+	while (str2[++size])
+		dest[i + size] = str2[size];
 	dest[i + size] = 0;
-	free(str1);
+	//printf("dest = |%s|\n", dest);
+	if (str1)
+		free(str1);
 	return (dest);
 }
 
-char	*ft_substr(char *str, int start, int end, int to_free)
+char	*get_line(char *str)
+{
+	char	*line;
+	int		i;
+	int		j;
+
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	i++;
+	line = malloc(sizeof(char) * (i + 1));
+	if (!line)
+		return (NULL);
+	j = -1;
+	while (++j < i)
+		line[j] = str[j];
+	line[j] = 0;
+	return (line);
+}
+
+char	*ft_strdup(char *str)
 {
 	char	*dest;
 	int		i;
 
-	if (end < 1)
-	{
-		//printf("salut\n");
-		end = ft_strlen(str);
-	}
-
-	if (!str || start < 0 || start >= end || end > ft_strlen(str))
-	{
-		if (to_free)
-			free(str);
+	if (!str)
 		return (NULL);
-	}
-	dest = malloc(sizeof(char) * (end - start + 1));
+	dest = malloc(sizeof(char) * ft_strlen(str) + 1);
 	if (!dest)
 		return (NULL);
 	i = -1;
-	while (str[start + ++i] && (start + i) < end)
-		dest[i] = str[start + i];
+	while (str[++i])
+		dest[i] = str[i];
 	dest[i] = 0;
-	if (to_free)
-		free(str);
 	return (dest);
+}
+
+char	*get_after_line(char *save)
+{
+	char	*dest;
+	int		i;
+
+	i = -1;
+	while (save[++i])
+	{
+		if (save[i] == '\n')
+		{
+			dest = ft_strdup(save + i + 1);
+			free(save);
+			return (dest);
+		}
+	}
+	free(save);
+	return (NULL);
 }
 
 char	*get_next_line(int fd)
@@ -109,16 +135,27 @@ char	*get_next_line(int fd)
 	while (ft_index(save, '\n') < 0 && reader)
 	{
 		reader = read(fd, buff, BUFFER_SIZE);
-		buff[reader] = 0;
-		save = ft_gnljoin(save, buff);
-		if (!save)
+		if (reader < 0)
+		{
+			if (save)
+				free(save);
+			free(buff);
 			return (NULL);
+		}
+		buff[reader] = 0;
+		save = gnljoin(save, buff);
 	}
 	free(buff);
-	line = ft_substr(save, 0, ft_index(save, '\n') + 1, 0);
-	save = ft_substr(save, ft_index(save, '\n') + 1, ft_strlen(save), 1);
+	if (!reader)
+	{
+		if (save)
+			free(save);
+		return (NULL);
+	}
+	line = get_line(save);
+	save = get_after_line(save);
+	//printf("line = |%s|\n", save);
+	//printf("salut\n");
 	return (line);
 }
 
-
-// la commende : clear && c *.c && ./a.out | cat -e
--- exam02/ex01/gnl/main.c
+++ exam02/ex01/gnl/main.c
@@ -6,7 +6,7 @@
 /*   By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 18:56:31 by zminhas           #+#    #+#             */
-/*   Updated: 2021/11/04 20:59:41 by zminhas          ###   ########.fr       */
+/*   Updated: 2021/11/10 17:28:04 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,15 @@ int main(void)
 {
 	char	*str;
 	int	fd;
-	int	roll;
 
 	fd = open("text.txt", O_RDONLY);
-	roll = 8;
-	while (roll--)
+	while ((str = get_next_line(fd)))
 	{
-		str = get_next_line(fd);
 		printf("%s", str);
 		free(str);
 	}
 	close(fd);
 	return (0);
 }
+
+// la commende : clear && c *.c && ./a.out | cat -e
