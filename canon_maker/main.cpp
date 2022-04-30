36e1f742b446500b9c3504e1fa6c29133771e40f
ZaineMinhas <zaineminhas5@gmail.com>
2022-04-30 16:28:00 +0200

    GROS COMMIT POUR GROS PUSH

--- canon_maker/main.cpp
+++ canon_maker/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zminhas <zminhas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 17:48:23 by zminhas           #+#    #+#             */
-/*   Updated: 2022/04/27 18:50:52 by zminhas          ###   ########.fr       */
+/*   Updated: 2022/04/30 16:24:14 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,15 @@ int	return_error(void)
 
 int	main(int ac, char **av)
 {
-	if (ac != 2)
+	if (ac != 3)
 		return (return_error());
-	canon_maker	make(av[1]);
+	canon_maker	make(av[2]);
 	if (make.canon())
 		return (return_error());
+	std::string	hpp = "mv " + (std::string)av[2] + ".hpp " + av[1];
+	std::string	cpp = "mv " + (std::string)av[2] + ".cpp " + av[1];
+	system(hpp.c_str());
+	system(cpp.c_str());
 	return (0);
 }
 
